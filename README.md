5c562e05a7396e84088b93823d326d8828525492
johna <johnchang9094@gmail.com>
2020-04-29 22:44:09 +0100

    README and fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Canvas
-Coding Problem: Drawing Program
+## Coding Problem: Canvas
+- This program creates a Canvas and draws lines and rectangles corresponding to the coordinates given.
+
+## Run the project
+- Install Maven if not installed already: https://maven.apache.org/install.html
+- Clone the project
+- From the root directory, run `mvn clean package`, this will clean the project and package the project into an executable jar file
+- The jar will be created under the folder `target/Canvas-1.0.0-shaded.jar` which 
+you can run the program
+
+- To run the program, run use the following command in the root directory:
+`java -jar target/Canvas-1.0.0-shaded.jar`
+- The program will run telling you to enter the command. This means you have successfully built the project!
+
+### Program instructions
+
+![Alt text](images/task definition.jpg)
+
+- To build an initial canvas, run `C 20 5`. This will build a canvas of width 20 and length 5
+- To create a line, run `L 1 3 7 3`. This will draw a line beginning from `(1, 3)` to `(7, 3)`
+- To create an adjacent line, run `L 7 1 7 3`. This will draw a line beginning from `(7, 1)` to `(7, 3)`
+- To create a rectangle, run `R 15 2 20 5`. This will draw a rectangle with the upper left point starting
+from `(15, 2)` and ending at coordinate `(20, 5)`
+- Enter `Q` to exit
+
+### Example output
+![Alt text](images/program output.jpg)
--- src/main/java/com/johnc/Program.java
+++ src/main/java/com/johnc/Program.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Program {
 
     private CanvasService canvasService;
+    private String canvas;
 
     public Program(CanvasService canvasService) {
         this.canvasService = canvasService;
@@ -16,43 +17,52 @@ public class Program {
         String input = sc.nextLine();
 
         while (!input.equals("Q")) {
-            try {
-                String[] inputArray = input.split(" ");
-                if (inputArray.length == 3 && inputArray[0].equals("C")) {
-                    int width = Integer.parseInt(inputArray[1]);
-                    int height = Integer.parseInt(inputArray[2]);
-                    canvasService.createCanvas(width, height);
-                    String canvas = canvasService.getCanvas();
-                    System.out.println(canvas);
-                } else {
-                    System.out.println("please enter the shape (\"C\", \"L\", R\") and the corresponding coordinates");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Please enter a valid number for each coordinate for the canvas");
-            }
-
-            System.out.print("enter command: ");
-            Scanner sc2 = new Scanner(System.in);
-            input = sc2.nextLine();
-            String[] secondInputArray = input.split(" ");
-
-            try {
-                if (secondInputArray.length == 5) {
-                    int x1 = Integer.parseInt(secondInputArray[1]);
-                    int y1 = Integer.parseInt(secondInputArray[2]);
-                    int x2 = Integer.parseInt(secondInputArray[3]);
-                    int y2 = Integer.parseInt(secondInputArray[4]);
-                    if (secondInputArray[0].equals("L")) {
-                        canvasService.drawShape(new Line(x1, y1, x2, y2));
-                    } else if (secondInputArray[0].equals("R")) {
-                        canvasService.drawShape(new Rectangle(x1, y1, x2, y2));
+            if (canvas == null) {
+                try {
+                    String[] inputArray = input.split(" ");
+                    if (inputArray.length == 3 && inputArray[0].equals("C")) {
+                        int width = Integer.parseInt(inputArray[1]);
+                        int height = Integer.parseInt(inputArray[2]);
+                        canvasService.createCanvas(width, height);
+                        canvas = canvasService.getCanvas();
+                        System.out.println(canvas);
+                    } else {
+                        System.out.println("please enter the shape (\"C\", \"L\", R\") and the corresponding coordinates");
+                        System.out.print("enter command: ");
+                        input = sc.nextLine();
                     }
-                } else {
-                    System.out.println("please enter the shape (\"C\", \"L\", R\") and the corresponding coordinates");
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a valid number for each coordinate for the canvas");
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Please enter a valid number for each coordinate for the shape");
+            } else {
+                System.out.print("enter command: ");
+                input = sc.nextLine();
+                String[] secondInputArray = input.split(" ");
+
+                if (input.equals("Q")) {
+                    break;
+                }
+
+                try {
+                    if (secondInputArray.length == 5) {
+                        int x1 = Integer.parseInt(secondInputArray[1]);
+                        int y1 = Integer.parseInt(secondInputArray[2]);
+                        int x2 = Integer.parseInt(secondInputArray[3]);
+                        int y2 = Integer.parseInt(secondInputArray[4]);
+                        if (secondInputArray[0].equals("L")) {
+                            canvasService.drawShape(new Line(x1, y1, x2, y2));
+                        } else if (secondInputArray[0].equals("R")) {
+                            canvasService.drawShape(new Rectangle(x1, y1, x2, y2));
+                        }
+                    } else {
+                        System.out.println("please enter the shape (\"C\", \"L\", R\") and the corresponding coordinates");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a valid number for each coordinate for the shape");
+                }
             }
         }
+
+        System.out.println("Program terminated");
     }
 }
